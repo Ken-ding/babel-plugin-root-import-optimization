3a407c3e92b3054980000f686930c83c836095ec
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2021-01-19 16:13:16 +0800

    mod:修复window文件系统

--- plugin/helper.js
+++ plugin/helper.js
@@ -22,11 +22,11 @@ export const transformRelativeToRootPath = (
     const suffix = rootPathSuffix ? rootPathSuffix : './';
     const root = typeof _root === 'function' ? _root(sourceFile) : _root;
     const absolutePath = path.resolve(root, `${suffix}/${withoutRootPathPrefix}`);
-
+    let sourcePath;
     if(sourceFile.indexOf('/')==-1){
-        let sourcePath = sourceFile.substring(0, sourceFile.lastIndexOf('\\'));
+         sourcePath = sourceFile.substring(0, sourceFile.lastIndexOf('\\'));
     }else{
-        let sourcePath = sourceFile.substring(0, sourceFile.lastIndexOf('/'));
+        sourcePath = sourceFile.substring(0, sourceFile.lastIndexOf('/'));
     }
 
     let relativePath = path.relative(path.resolve(sourcePath), absolutePath).replace(/\\/g, '/');
